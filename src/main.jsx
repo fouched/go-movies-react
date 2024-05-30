24cdf3ddbd7b754393192d2871432335b1437ff4
Fouche du Preez <fouche@limehouse.co.za>
2024-05-30 19:23:21 +0200

    added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ErrorPage from "./components/ErrorPage.jsx";
+import Home from "./components/Home.jsx";
+import Movies from "./components/Movies.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {index: true, element: <Home />},
+            {
+                path: "/movies",
+                element: <Movies />,
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
