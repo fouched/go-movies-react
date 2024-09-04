0e013ad8355dfea9213928513c33a5a36bca510e
Fouche du Preez <fouche@limehouse.co.za>
2024-09-04 08:16:22 +0200

    added delete

--- src/components/EditMovie.jsx
+++ src/components/EditMovie.jsx
@@ -233,6 +233,38 @@ const EditMovie = () => {
 
     }
 
+    const confirmDelete = () => {
+        Swal.fire({
+            title: "Delete movie?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let headers = new Headers();
+                headers.append("Authorization", "Bearer " + jwtToken);
+
+                const requestOptions = {
+                    method: "DELETE",
+                    headers: headers,
+                }
+                fetch(`http://localhost:9080/admin/movies/${movie.id}`, requestOptions)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        if (data.error) {
+                            console.log(data.error);
+                        } else {
+                            navigate("/manage-catalogue")
+                        }
+                    })
+                    .catch(err => console.log(err));
+            }
+        });
+    }
+
     if (error !== null) {
         return <div>Error: {error.message}</div>
     } else {
@@ -319,7 +351,9 @@ const EditMovie = () => {
                         <hr/>
 
                         <button className="btn btn-primary">Save</button>
-
+                        {movie.id > 0 &&
+                            <a href="#" className="btn btn-danger ms-2" onClick={confirmDelete}>Delete</a>
+                        }
                     </form>
                 </div>
             </>
